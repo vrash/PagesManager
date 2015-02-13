44d92ba107c2377085936142e3522bc87497947a
vrashabh <vrashabh86@gmail.com>
2015-02-13 12:40:34 +0530

    Nothing important yet

--- app/src/main/java/vrashabh/fbpagesmanager/LoginActivity.java
+++ app/src/main/java/vrashabh/fbpagesmanager/LoginActivity.java
@@ -1,25 +1,73 @@
 package vrashabh.fbpagesmanager;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
+import com.facebook.Request;
+import com.facebook.Response;
+import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.model.GraphUser;
 import com.facebook.widget.LoginButton;
 
 
-public class LoginActivity extends ActionBarActivity {
+public class LoginActivity extends ActionBarActivity implements View.OnClickListener {
+
+    Context mContext = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login2);
-
         LoginButton fbLogin = (LoginButton) findViewById(R.id.authButton);
 
     }
 
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Session.getActiveSession().onActivityResult(this, requestCode,
+                resultCode, data);
+    }
+
+    @Override
+    public void onClick(View v) {
+        // start Facebook Login
+        Session.openActiveSession(this, true, new Session.StatusCallback() {
+
+            // callback when session changes state
+            @Override
+            public void call(Session session, SessionState state,
+                             Exception exception) {
+                if (session.isOpened()) {
+
+                    // make request to the /me API
+                    Request.executeMeRequestAsync(session,
+                            new Request.GraphUserCallback() {
+
+                                // callback after Graph API response with user
+                                // object
+                                @Override
+                                public void onCompleted(GraphUser user,
+                                                        Response response) {
+                                    if (user != null) {
+                                        //TextView welcome = (TextView) findViewById(R.id.welcome);
+                                        Toast.makeText(mContext, "Hello "
+                                                + user.getName() + "!", Toast.LENGTH_LONG).show();
+                                    }
+                                }
+                            });
+                }
+            }
+        });
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -41,5 +89,15 @@ public class LoginActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        Session session = Session.getActiveSession();
+        if (session!=null && session.isOpened()) {
+            Toast.makeText(mContext, session.getAccessToken(), Toast.LENGTH_LONG).show();
+        }
     }
 }
---
+++ app/src/main/java/vrashabh/fbpagesmanager/utilities/FBPagesManager.java
@@ -0,0 +1,16 @@
+package vrashabh.fbpagesmanager.utilities;
+
+import android.app.Application;
+
+/**
+ * Created by vrashabh on 2/13/15.
+ */
+public class FBPagesManager extends Application {
+
+    //Facebook key goodness
+    String fbAppKey;
+    String fbAppSecret;
+    String fbSessionAuth;
+
+
+}
